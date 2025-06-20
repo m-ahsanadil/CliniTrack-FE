f12174fbccdaa305e97c6b0bfde64833b99d73de
Muhammad Faizan Adil <adilmuhammadfaizan@gmail.com>
2025-06-20 20:33:36 +0500

    added routes pages

---
+++ app/(DASHBOARD)/layout.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react"
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: ReactNode
+}) {
+  return (
+    <div className="dashboard-layout">
+      {/* youtube dashboard layout */}
+        {children}
+    </div>
+  )
+}
---
+++ app/(DASHBOARD)/medical-records/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MedicalRecordsPage() {
+  return (
+    <div>PatientsPage</div>
+  )
+}
