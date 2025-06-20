b977f9eb80fd9d30dd455c8c89c41014acc279b5
Muhammad Faizan Adil <adilmuhammadfaizan@gmail.com>
2025-06-20 22:17:52 +0500

    modified

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,14 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/login',
+      },
+    ];
+  },
 }
 
 export default nextConfig
