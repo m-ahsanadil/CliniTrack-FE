8f135613cbbe462f34f0498a248590851efc7f7e
Muhammad Faizan Adil <adilmuhammadfaizan@gmail.com>
2025-06-21 06:31:00 +0500

    deleted files

--- src/modules/Authentication/login/api/api.ts
+++
@@ -1,9 +0,0 @@
-import { ENDPOINTS } from "@/src/redux/config/api";
-import { LoginApiResponse, LoginRequest } from "./types";
-import apiService from "@/src/redux/config/apiService";
-
-export const loginApi = {
-        login: (credentials: LoginRequest): Promise<LoginApiResponse> => {
-                return apiService.post(ENDPOINTS.AUTH.LOGIN, credentials);
-        }
-}
